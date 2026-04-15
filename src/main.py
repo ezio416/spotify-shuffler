23c7d89a739d8fee4706ac394d58000c33134cf8
Ezio <e@e416.dev>
2026-04-15 01:40:21 -0600

    init

---
+++ src/main.py
@@ -0,0 +1,212 @@
+import base64
+import datetime
+import json
+import os
+import time
+import webbrowser
+
+import pytz
+import requests
+
+
+AUTH_BASE_URL          = 'https://accounts.spotify.com/api'
+AUTH_CONTENT_TYPE      = 'application/x-www-form-urlencoded'
+BASE_URL               = 'https://api.spotify.com/v1'
+CODE_GOOD              = 200
+CODE_CREATED           = 201
+CODE_ACCEPTED          = 202
+CODE_NO_CONTENT        = 204
+CODE_NOT_MODIFIED      = 304
+CODE_BAD_REQUEST       = 400
+CODE_UNAUTHORIZED      = 401
+CODE_FORBIDDEN         = 403
+CODE_NOT_FOUND         = 404
+CODE_TOO_MANY_REQUESTS = 429
+CODE_INTERNAL_ERROR    = 500
+CODE_BAD_GATEWAY       = 502
+CODE_UNAVAILABLE       = 503
+PERMISSIONS            = ('user-modify-playback-state', 'user-read-playback-state')
+REDIRECT_URI           = 'http://127.0.0.1:7777/callback'
+SCRIPT_DIR             = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+AUTH_FILE              = os.path.join(SCRIPT_DIR, 'auth.json')
+CONFIG_FILE            = os.path.join(SCRIPT_DIR, 'config.json')
+LOG_FILE               = os.path.join(SCRIPT_DIR, 'log.log')
+WAIT_TIME_MS           = 2000
+
+active_device_id = ''
+auth             = {}
+auth_code        = ''
+config           = {}
+last_refresh     = 0
+last_request     = 0
+shuffle          = False
+
+
+def get_auth() -> bool:
+    global auth
+
+    req = requests.post(
+        f'{AUTH_BASE_URL}/token'
+            + '?grant_type=authorization_code'
+            + f'&code={auth_code}'
+            + f'&redirect_uri={REDIRECT_URI}',
+        headers={
+            'Authorization': auth['basic'],
+            'Content-Type': AUTH_CONTENT_TYPE
+        }
+    )
+
+    if req.status_code != CODE_GOOD:
+        log(f'failed getting auth ({req.status_code}): {req.text}')
+        return False
+
+    try:
+        data = req.json()
+        auth['access'] = f'{data['token_type']} {data['access_token']}'
+        auth['refresh'] = data['refresh_token']
+        log('got auth')
+        return True
+
+    except Exception as e:
+        log(f'failed getting auth: {e}')
+        return False
+
+
+def get_auth_code() -> bool:
+    global auth_code
+    global config
+
+    webbrowser.open(
+        'https://accounts.spotify.com/authorize'
+            + f'?client_id={config['client_id']}'
+            + '&response_type=code'
+            + f'&redirect_uri={REDIRECT_URI}'
+            + f'&scope={' '.join(PERMISSIONS)}'
+    )
+
+    try:
+        auth_code = input('callback URL: ').split('?code=')[1]
+    except Exception:
+        pass
+
+    return len(auth_code) > 0
+
+
+def get_devices():
+    global auth
+
+    ...
+
+
+def get_playback_state():
+    global auth
+
+    ...
+
+
+def handle_new_device():
+    global auth
+
+    ...
+
+
+def load_auth() -> bool:
+    global auth
+
+    try:
+        with open(AUTH_FILE) as f:
+            auth = json.load(f)
+
+            return all((
+                'access' in auth,
+                'basic' in auth,
+                'refresh' in auth
+            ))
+
+    except Exception:
+        return False
+
+
+def load_config() -> bool:
+    global config
+
+    try:
+        with open(CONFIG_FILE) as f:
+            config = json.load(f)
+
+            return all((
+                'client_id' in config,
+                'client_secret' in config
+            ))
+
+    except Exception:
+        return False
+
+
+def log(msg: str) -> None:
+    utc = datetime.datetime.now(pytz.timezone('UTC')).strftime(f'%Y-%m-%d %H:%M:%S.%f')[:-3]
+    denver = f'Denver {datetime.datetime.now(pytz.timezone('America/Denver')).strftime('%H:%M')}'
+    text = f'[{utc} ({denver})] {msg}'
+
+    print(text)
+
+    with open(LOG_FILE, 'a', newline='\n') as f:
+        f.write(f'{text.encode('unicode-escape').decode('ascii')}\n')
+
+
+def refresh_auth() -> bool:
+    global auth
+
+    ...
+
+
+def save_auth() -> bool:
+    global auth
+
+    try:
+        with open(AUTH_FILE, 'w', newline='\n') as f:
+            json.dump(auth, f, indent=4, sort_keys=True)
+            return True
+
+    except Exception:
+        return False
+
+
+def toggle_shuffle() -> bool:
+    global auth
+
+    ...
+
+
+def wait() -> None:
+    ...
+
+
+def main() -> None:
+    if not load_auth():
+        log('getting auth')
+
+        if not load_config():
+            log('bad/missing config')
+            return
+
+        auth['basic'] = f'Basic {base64.b64encode(f'{config['client_id']}:{config['client_secret']}'.encode()).decode()}'
+
+        if not get_auth_code():
+            log('missing auth code')
+            return
+
+        if not get_auth():
+            return
+
+        if not save_auth():
+            log('failed to save auth')
+            return
+
+    log('loaded auth')
+
+    ...
+
+
+if __name__ == '__main__':
+    main()
